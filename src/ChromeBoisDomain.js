3d572431575ef6f421807a4bdf0c40eecbe38060
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-03 08:51:17 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -11,8 +11,8 @@ export default class ChromeBoisDomain extends Component {
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
      
-     let x=event.cX
-     let y = event.cY;
+     let x=event.clientX
+     let y = event.clientY;
      drawChromeBoiAtCoords(x,y)
   }
   
